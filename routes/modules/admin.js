0043d7319e0bcc3f458eaffd840f26185edbdd34
ChingG0 <www.karta2428620@gmail.com>
2022-11-09 14:58:59 +0800

    feat: user model add is_admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const adminController = require('../../controllers/admin-controllers')
 
-router.get('/restaurants', adminController.getRestaurants)
+const { authenticatedAdmin } = require('../../middleware/auth')
+
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
