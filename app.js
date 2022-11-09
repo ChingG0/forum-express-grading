931c92b01ff6abf1c96e1271859dc5b2add4114d
ChingG0 <www.karta2428620@gmail.com>
2022-11-09 12:51:45 +0800

    feat: npm session, passport, connect-flash

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', exphbs({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_message = req.flash('success_message')
+  res.locals.error_message = req.flash('error_message')
+  next()
+})
+
 app.use(routes)
 
 app.listen(port, () => {
